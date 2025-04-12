6d0d54105c4df862f034ebf1f9830714ba9f3710
srs4929 <149858942+srs4929@users.noreply.github.com>
2025-04-12 09:19:52 +0600

    Update practice.sql

--- Lab-2/practice.sql
+++ Lab-2/practice.sql
@@ -1,1 +1,21 @@
+--creating table
+--branch table
+--T10 style
+create table branch(          
+   branch_name varchar(15)  primary key,
+   branch_city varchar(15) not null,
+   assets number check(assets>=10000)  -- checking assets value. If the value less than 10000 is given will show error
+);
+-- creating customer table
+--T20 style
+create table customer
+(
+   customer_id varchar(5) ,
+   customer_name varchar2(15) ,
+   customer_street varchar2(12) ,
+   customer_city varchar2(12)  not null,
+   cell varchar2(11) unique,
+   dob date not null,
+   primary key(customer_id)  
+);
 
