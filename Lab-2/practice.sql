e4be55b554df8b7d45139eebffe2b8bb806dfa7e
srs4929 <149858942+srs4929@users.noreply.github.com>
2025-04-12 10:20:38 +0600

    Update practice.sql

--- Lab-2/practice.sql
+++ Lab-2/practice.sql
@@ -32,3 +32,43 @@ create table account
   constraints bal check(balance>=0),
   constraints ch_ac_no check(account_no like'A-%')  
 );
+
+--creating loan table
+create table loan
+(loan_no char(5),
+branch_name varchar2(15),
+amount number(10,2) not null,
+constraint pk_loan_no primary key (loan_no),
+constraint fk_br_nm foreign key (branch_name) references branch(branch_name),
+constraint ch_amount check (amount >= 0),
+constraint ch_lo_no check (loan_no like 'L-%'));
+
+--creating depositor table
+create table depositor
+(
+   customer_id varchar2(5) ,
+   account_no char(5) ,
+   constraint pk_ci_an primary key(customer_id,account_no) , -- here for identifying primary key we need two key and both of the key is primary key of another table
+   constraint fk_ci foreign key (customer_id)  references customer(customer_id),
+   constraint fk_ac_no foreign key (account_no)  references account(account_no)
+);
+
+-- creating borrower table
+create table borrower
+(
+   customer_id varchar2(5),
+   loan_no char(5),
+   constraint pk_cui_lo primary key(customer_id,loan_no) ,
+   constraint fk_cui foreign key(customer_id) references customer(customer_id),
+   constraint fk_lo foreign key (loan_no)  references loan(loan_no)
+);
+
+--describe table
+describe borrower -- it will show table details
+-- Output:
+-- NAME         NULL?     TYPE
+-- ------------ --------- ----------
+-- CUSTOMER_ID  NOT NULL  VARCHAR2(5)
+-- LOAN_NO      NOT NULL  CHAR(5)
+
+--Since customer_id and loan_no both are primary key they can not be null .
