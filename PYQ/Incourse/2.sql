e3b3f06aa98573f967b1fa3ad33ca682081df926
srs4929 <149858942+srs4929@users.noreply.github.com>
2025-05-10 22:33:04 +0600

    Update 2.sql

--- PYQ/Incourse/2.sql
+++ PYQ/Incourse/2.sql
@@ -2,15 +2,15 @@ create table employee
 (
     id varchar(4),
     person_name varchar(10),
-    street varchar(7),
-    city varchar(8),
+    street varchar(20),
+    city varchar(20),
     constraint pk_id primary key (id)
 )
 
 create table works
 (
-    id varchar(2),
-    branch_name varchar(5),
+    id varchar(10),
+    branch_name varchar(20),
     salary number(10,0),
     constraint wo_id foreign key (id) references employee(id),
     foreign key (branch_name) references company(branch_name),
@@ -20,14 +20,14 @@ create table works
 
 create table company
 (
-    branch_name varchar(5),
-    city varchar(5),
+    branch_name varchar(10),
+    city varchar(10),
     primary key (branch_name)
 )
 
 create table manages
 (
-    id varchar(4),
-    manager_id varchar(5),
+    id varchar(10),
+    manager_id varchar(15),
     foreign key (id) references employee(id) 
 )
