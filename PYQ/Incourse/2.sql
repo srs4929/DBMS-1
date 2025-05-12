f65a6d53160a43ba01f8a7b77fa583e734001066
srs4929 <149858942+srs4929@users.noreply.github.com>
2025-05-13 04:55:36 +0600

    Update 2.sql

--- PYQ/Incourse/2.sql
+++ PYQ/Incourse/2.sql
@@ -31,3 +31,23 @@ create table manages
     manager_id varchar(15),
     foreign key (id) references employee(id) 
 )
+
+--a) Modify the database so that employee whose id is '12345' now works in a branch located in 'Newtown'
+update works
+set branch_name='Newtown'
+where id='12345'
+--b) Give each manager of "First bank corporation" a 10 percent raise unless the salary becomes greater than Tk.10,0000 unless give a 3 percent raise
+update works
+set salary = case
+                when salary * 1.1 < 100000 then salary * 1.1
+                else salary * 1.03
+             end
+where id in  (
+    select manager_id
+    from  manages
+    where manager_id in (
+        select id
+        from  works
+        where branch_name = 'First bank Corporation'
+    )
+);
