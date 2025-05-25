3e18e189a894daff6ac62c6d1ecf654c721c1bc7
srs4929 <149858942+srs4929@users.noreply.github.com>
2025-05-25 14:49:39 +0600

    Update work.sql

--- Lab-5/work.sql
+++ Lab-5/work.sql
@@ -1,9 +1,9 @@
-
+--process-1
 select dept_name,avg(salary)
 from instructor
 group by dept_name
 having avg(salary)>42000
-
+--process 2
 with deptnew(dept_name,avg_salary) as
 (
     select dept_name,avg(salary)
@@ -12,3 +12,8 @@ with deptnew(dept_name,avg_salary) as
 )
 select dept_name,avg_salary
 from deptnew where avg_salary>42000
+
+--process 3
+select dept_name,avg_salary
+from (select dept_name, avg(salary) as avg_salary from instructor group by dept_name)
+where avg_salary>42000
