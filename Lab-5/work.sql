b428ce7f034c235aab1959e83757fa38e8f14a20
srs4929 <149858942+srs4929@users.noreply.github.com>
2025-05-25 15:12:47 +0600

    Update work.sql

--- Lab-5/work.sql
+++ Lab-5/work.sql
@@ -17,3 +17,28 @@ from deptnew where avg_salary>42000
 select dept_name,avg_salary
 from (select dept_name, avg(salary) as avg_salary from instructor group by dept_name)
 where avg_salary>42000
+
+
+--process-1
+
+with newtable(dept_name,avg_salary) as
+(
+    select dept_name,avg(salary)
+    from instructor
+    group by dept_name
+)
+select name,salary,avg_salary
+from instructor,newtable
+where instructor.dept_name=newtable.dept_name
+
+--process-2
+select name,salary,avg_salary
+from (select dept_name,avg(salary) as avg_salary from instructor group by dept_name) natural join instructor
+--process-3
+select name,salary,(select avg(salary) as avg_salary from instructor s1 where s1.dept_name=s2.dept_name)
+from instructor s2
+
+
+
+
+
